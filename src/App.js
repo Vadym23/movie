ce8b564d23d8fff07c8df4b1fc15d55c8f54ffe4
Vadym23 <35969015+Vadym23@users.noreply.github.com>
2020-03-15 22:04:56 +0200

    a list of movie titles

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { moviesData } from './moviesData';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: moviesData
+    };
+  }
+
+  render() {
+    console.log(this);
+    return (
+      <div>
+        {this.state.movies.map(
+          function(movie) {
+            return (
+              <p>
+                {movie.title}
+              </p>
+            );
+          }
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
